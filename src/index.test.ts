5aa71555695bfee8a30b812a5e095ef2b5d9f62f
Li Kai <hello+github@lik.ai>
2020-10-08 03:14:12 +0800

    Add basic preact component compiler

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import { transpile } from "./index";
+import { transpile, LibraryType } from "./index";
 
 describe("transpile", () => {
   it("transforms a simple react component to react component", () => {
@@ -9,8 +9,27 @@ describe("transpile", () => {
       }
     `.trim();
 
-    expect(transpile(simpleComponent).outputText).toMatchInlineSnapshot(`
+    expect(transpile(simpleComponent, LibraryType.React).outputText)
+      .toMatchInlineSnapshot(`
       "import React from 'react';
+      export default function Component() {
+          return <div>hello world</div>;
+      }
+      "
+    `);
+  });
+
+  it("transforms a simple react component to preact component", () => {
+    const simpleComponent = `
+      import React from 'react';
+      export default function Component() {
+        return <div>hello world</div>;
+      }
+    `.trim();
+
+    expect(transpile(simpleComponent, LibraryType.Preact).outputText)
+      .toMatchInlineSnapshot(`
+      "import { h } from 'preact';
       export default function Component() {
           return <div>hello world</div>;
       }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import ts from 'typescript';
+import ts from "typescript";
 
 const DEFAULT_COMPILE_OPTIONS: Readonly<ts.CompilerOptions> = {
   allowJs: true,
@@ -11,7 +11,70 @@ const DEFAULT_COMPILE_OPTIONS: Readonly<ts.CompilerOptions> = {
   jsx: ts.JsxEmit.Preserve,
 };
 
-function nodeVisitor(ctx: ts.TransformationContext, sf: ts.SourceFile) {
+export enum LibraryType {
+  React = "react",
+  Preact = "preact",
+}
+
+const REACT_REGEX = /['"]react['"]/;
+const SINGLE_QUOTE_REGEX = /'/;
+
+/**
+ * Check if this node is a import react node
+ *
+ * @param {ts.Node} node node
+ * @param {ts.SourceFile} sf source file to get text from
+ * @returns {boolean} true if it is, false otherwise
+ */
+function isReactImport(
+  node: ts.Node,
+  sf: ts.SourceFile,
+): node is ts.ImportDeclaration {
+  return (
+    ts.isImportDeclaration(node) &&
+    REACT_REGEX.test(node.moduleSpecifier.getText(sf))
+  );
+}
+
+function isSingleQuote(node: ts.Node, sf: ts.SourceFile): boolean {
+  return (
+    ts.isStringLiteralLike(node) && SINGLE_QUOTE_REGEX.test(node.getText(sf))
+  );
+}
+
+function nodeVisitor(
+  ctx: ts.TransformationContext,
+  sf: ts.SourceFile,
+  libType: LibraryType,
+) {
+  if (libType === LibraryType.Preact) {
+    const preactVisitor: ts.Visitor = (node) => {
+      if (isReactImport(node, sf)) {
+        const hImportSpecifier = ts.factory.createImportSpecifier(
+          undefined,
+          ts.factory.createIdentifier("h"),
+        );
+        return ts.factory.updateImportDeclaration(
+          node,
+          undefined,
+          undefined,
+          ts.factory.createImportClause(
+            false,
+            undefined,
+            ts.factory.createNamedImports([hImportSpecifier]),
+          ),
+          ts.factory.createStringLiteral(
+            "preact",
+            isSingleQuote(node.moduleSpecifier, sf),
+          ),
+        );
+      }
+      return ts.visitEachChild(node, preactVisitor, ctx);
+    };
+
+    return preactVisitor;
+  }
+
   const visitor: ts.Visitor = (node) => {
     return ts.visitEachChild(node, visitor, ctx);
   };
@@ -19,23 +82,33 @@ function nodeVisitor(ctx: ts.TransformationContext, sf: ts.SourceFile) {
   return visitor;
 }
 
-export function transform(): ts.TransformerFactory<ts.SourceFile> {
-  return (ctx) => (sf) => ts.visitNode(sf, nodeVisitor(ctx, sf));
+export function transform(
+  libType: LibraryType,
+): ts.TransformerFactory<ts.SourceFile> {
+  return (ctx) => (sf) => ts.visitNode(sf, nodeVisitor(ctx, sf, libType));
 }
 
-export function transpile(input: string, options?: ts.CompilerOptions): ts.TranspileOutput {
-  const mergedOptions = {...options, ...DEFAULT_COMPILE_OPTIONS};
+export function transpile(
+  input: string,
+  libType: LibraryType,
+  compilerOptions?: ts.CompilerOptions,
+): ts.TranspileOutput {
+  const mergedOptions = { ...compilerOptions, ...DEFAULT_COMPILE_OPTIONS };
   let emitResult = ts.transpileModule(input, {
     compilerOptions: mergedOptions,
     transformers: {
-      before: [transform()],
+      before: [transform(libType)],
     },
   });
   return emitResult;
 }
 
-function compile(fileNames: string[], options?: ts.CompilerOptions): void {
-  const mergedOptions = {...options, ...DEFAULT_COMPILE_OPTIONS};
+function compile(
+  fileNames: string[],
+  libType: LibraryType,
+  compilerOptions?: ts.CompilerOptions,
+): void {
+  const mergedOptions = { ...compilerOptions, ...DEFAULT_COMPILE_OPTIONS };
   let program = ts.createProgram(fileNames, mergedOptions);
   let emitResult = program.emit(
     undefined,
@@ -45,8 +118,8 @@ function compile(fileNames: string[], options?: ts.CompilerOptions): void {
     undefined,
     undefined,
     {
-      before: [transform()],
-    }
+      before: [transform(libType)],
+    },
   );
 
   let allDiagnostics = ts
@@ -59,11 +132,11 @@ function compile(fileNames: string[], options?: ts.CompilerOptions): void {
   allDiagnostics.forEach((diagnostic) => {
     if (diagnostic.file) {
       let { line, character } = diagnostic.file.getLineAndCharacterOfPosition(
-        diagnostic.start!
+        diagnostic.start!,
       );
       let message = ts.flattenDiagnosticMessageText(
         diagnostic.messageText,
-        '\n'
+        "\n",
       );
 
       let logMethod;
@@ -79,11 +152,13 @@ function compile(fileNames: string[], options?: ts.CompilerOptions): void {
       }
 
       logMethod(
-        `${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`
+        `${diagnostic.file.fileName} (${line + 1},${
+          character + 1
+        }): ${message}`,
       );
     } else {
       console.log(
-        ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n')
+        ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n"),
       );
     }
   });
@@ -98,5 +173,5 @@ function compile(fileNames: string[], options?: ts.CompilerOptions): void {
 if (require.main === module) {
   // TODO: pass compile options via flags
   // TODO: help output
-  compile(process.argv.slice(2));
+  compile(process.argv.slice(3), process.argv[2] as LibraryType);
 }
